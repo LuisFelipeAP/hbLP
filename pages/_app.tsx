0aa06ace61301dfa7b254ea40675c79d4cf74c64
LuisFelipeAP <lusfelipea@gmail.com>
2022-12-19 15:39:04 -0300

    feat: added live bot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,31 @@ export default function App({ Component, pageProps }: AppProps) {
 				<FadeContextProvider>
 					<Component {...pageProps} />
 				</FadeContextProvider>
+				<Script
+					id='landbot-livechat'
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: 
+						`
+						window.addEventListener('mouseover', initLandbot, { once: true });
+						window.addEventListener('touchstart', initLandbot, { once: true });
+						var myLandbot;
+						function initLandbot() {
+						  if (!myLandbot) {
+							var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+							s.addEventListener('load', function() {
+							  var myLandbot = new Landbot.Livechat({
+								configUrl: 'https://storage.googleapis.com/landbot.site/v3/H-1417971-45BTTSPT7VPSI4AV/index.json',
+							  });
+							});
+							s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+							var x = document.getElementsByTagName('script')[0];
+							x.parentNode.insertBefore(s, x);
+						  }
+						}
+						`
+					}}
+				/>
 			</AppProvider>
 		</>
 	)
